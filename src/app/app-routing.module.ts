2bb3d1624c5832b5b68f1c55491e5e606b8e909c
rishabh <rishabh.webllisto@gmail.com>
2019-11-01 17:14:49 +0530

    change in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { SitemapComponent } from './sitemap/sitemap.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'home', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+{ path: 'contact', component:ContactComponent },
+{ path: 'blog', component: BlogComponent },
+{ path: 'privacy', component: PrivacyComponent },
+{ path: 'sitemap', component: SitemapComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { SitemapComponent } from './sitemap/sitemap.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    SitemapComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
